2769ba524cc993da0c4b1625abec7ad39cd2a5a6
XuntaoLyu <xuntaolyu@gmail.com>
2025-11-02 18:39:30 +0000

    fix

--- projectB/swi-prolog.pl
+++ projectB/swi-prolog.pl
@@ -10,19 +10,86 @@
 % facultyAffiliation(FacultyID,HomeDepartment)/2
 % planOfGraduateWorkApproved(StudentID)/1
 
-registrationSemester(s002, msc, fall, 2023, yes).
-registrationSemester(s002, msc, spring, 2024, no).
-registrationSemester(s002, msc, fall, 2024, no).
-registrationSemester(s002, msc, spring, 2025, no).
-registrationSemester(s002, msc, fall, 2025, no).
+% registration history
+registrationSemester(s003, phd, fall, 2020, yes).
+registrationSemester(s003, phd, spring, 2021, no).
+registrationSemester(s003, phd, fall, 2021, no).
+registrationSemester(s003, phd, spring, 2022, no).
+registrationSemester(s003, phd, fall, 2022, no).
+registrationSemester(s003, phd, spring, 2023, no).
+registrationSemester(s003, phd, fall, 2023, no).
+registrationSemester(s003, phd, spring, 2024, no).
+registrationSemester(s003, phd, fall, 2025, no).
 
-hasTakenCourse(s002, csc600, s001, 1, 3.3).
-hasTakenCourse(s002, csc503, s001, 3, 3.3).
-hasTakenCourse(s002, csc501, s001, 3, 3.0).
-hasTakenCourse(s002, csc565, s001, 3, 2.7).
-hasTakenCourse(s002, csc579, s001, 3, 2.7).
-hasTakenCourse(s002, csc630, s001, 3, 3.0).
-hasTakenCourse(s002, csc580, s001, 3, 3.0).
+% Fall 2020 (10 credits)
+% Orientation course
+hasTakenCourse(s003, csc600, s001, 1, 4.333).
+% Theory core
+hasTakenCourse(s003, csc505, s001, 3, 4.333).  
+% Systems core
+hasTakenCourse(s003, csc501, s001, 3, 4.333).  
+hasTakenCourse(s003, csc540, s001, 3, 4.333).  
+
+% Spring 2021 (12 credits)
+% Theory core
+hasTakenCourse(s003, csc503, s001, 3, 4.333).  
+% 700~ level
+hasTakenCourse(s003, csc714, s001, 3, 4.333).  
+hasTakenCourse(s003, csc712, s001, 3, 4.333). 
+% research credits
+hasTakenCourse(s003, csc830, s001, 3, 3.333).  
+
+% Fall 2021 (9 credits)
+% research credits
+hasTakenCourse(s003, csc830, s001, 3, 3.333).  
+% written prelim
+hasTakenCourse(s003, csc890, s001, 6, 3.333).  
+
+% Spring 2022 (12 credits)
+% research credits
+hasTakenCourse(s003, csc830, s001, 3, 3.333).  
+hasTakenCourse(s003, csc893, s001, 9, 3.333).
+
+% fall 2022 (9 credits, but only 3 credits toward graduaction)
+% oral prelim
+hasTakenCourse(s003, csc890, s001, 6, 3.333).
+% research credits
+hasTakenCourse(s003, csc830, s001, 3, 3.333).  
+
+% Spring 2023 (9 credits)
+% research credits
+hasTakenCourse(s003, csc893, s001, 9, 3.333).
+
+% fall 2023 (9 credits)
+% research credits
+hasTakenCourse(s003, csc893, s001, 9, 3.333).
+
+% Spring 2024 (9 credits)
+% dissertation
+hasTakenCourse(s003, csc895, s001, 9, 3.333).
+
+% Exam outcomes
+phdWrittenExamTaken(s003, fall, 2021, 3.333).
+phdOralExamTaken(s003, fall, 2022, 3.333).
+phdDefenseTaken(s003, spring, 2024, 3.333).
+
+% Graduate advisor
+graduateAdvisor(s003, advisor1, phd).
+
+% Advisory committee (4 members)
+advisoryCommitteeMember(s003, advisor1).
+advisoryCommitteeMember(s003, member2).
+advisoryCommitteeMember(s003, member3).
+advisoryCommitteeMember(s003, member4).
+
+% Faculty affiliations
+facultyAffiliation(advisor1, csc).
+facultyAffiliation(member2, csc).
+facultyAffiliation(member3, ece).  % Outside CS
+facultyAffiliation(member4, csc).
+
+% Plan approved
+planOfGraduateWorkApproved(s003).
 
 % Outcome is pass or fail 
 % current course offerings (CourseNumber, Section, MinUnits, MaxUnits, Prerequisite)
